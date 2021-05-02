27d77670a7198aba72c47d36cd7d7768cfe875ca
Elechim01 <mikymanny015@gmail.com>
2021-05-02 18:34:28 +0200

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by Michele Manniello on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
